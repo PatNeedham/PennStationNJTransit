54a0f866b62dc58bfd7ef3e4c2ad3dd1599d8dea
Pat Needham <pat@needham.io>
2018-06-17 14:42:37 -0400

    circleci setup

--- src/serverless/handler.js
+++ src/serverless/handler.js
@@ -1,10 +1,12 @@
 import {saveToS3} from './utils/s3'
 import {scrapeSite} from './utils/scraper'
+import {getTimestamp} from './utils/time'
 
 export async function scrape(event, context, callback) {
   let data = await scrapeSite()
   console.log('the data:')
   console.log(JSON.stringify(data, null, 2))
+  let Key = getTimestamp()
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -12,6 +14,5 @@ export async function scrape(event, context, callback) {
       input: event,
     }),
   }
-
   callback(null, response)
 }
