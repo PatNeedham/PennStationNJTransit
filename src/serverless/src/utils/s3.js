94864bc0e0bb7d5815ac306aa17c11e29a84e024
Pat Needham <pat@needham.io>
2018-06-19 21:18:40 -0400

    node denv in babel -> 8.10

--- src/serverless/src/utils/s3.js
+++ src/serverless/src/utils/s3.js
@@ -2,8 +2,8 @@ const AWS = require('aws-sdk')
 const {Buffer} = require('buffer')
 
 export async function saveToS3(Bucket, Key, obj) {
-  var s3 = new AWS.S3()
-  var Body = Buffer.from(JSON.stringify(obj, null, 2))
-  var params = {Bucket, Key, Body}
+  let s3 = new AWS.S3()
+  let Body = Buffer.from(JSON.stringify(obj, null, 2))
+  let params = {Bucket, Key, Body}
   return await s3.putObject(params).promise()
 }
